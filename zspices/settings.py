848ca0b5296f3c2f7196d6607267992429ac3e97
mqsaud <mqsaud@gmail.com>
2021-12-03 22:57:10 +0000

    Removed secret key and set debug

--- zspices/settings.py
+++ zspices/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)2*-w^rfzg+2*6f(t2z_@%o=vn9tt1ih0igyp&uouf*6nc03#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['zspices.herokuapp.com', 'localhost']
 
