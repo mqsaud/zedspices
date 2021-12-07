97adbee817157baf42a369ce43d4450998448b76
mqsaud <mqsaud@gmail.com>
2021-12-07 20:17:01 +0000

    Add rate and review form in product_details.html

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, RateAndReview
 
 # Register your models here.
 
@@ -30,3 +30,4 @@ class ProductAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(RateAndReview)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -39,4 +40,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+
+class RateAndReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    rating = models.FloatField()
+    review = models.TextField()
+    status = models.BooleanField()
+    created_on_date = models.DateTimeField(auto_now_add=True)
+    updated_on_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
