2fc743a55d2ef0f33a0e9d14c4472cd772127f6c
mqsaud <mqsaud@gmail.com>
2021-12-13 12:14:22 +0000

    Add contact.html

--- home/views.py
+++ home/views.py
@@ -7,6 +7,12 @@ def index(request):
 
     return render(request, 'home/index.html')
 
+
 def about(request):
     """ View to return about page"""
     return render(request, 'home/about.html')
+
+
+def contact(request):
+    """ View to return contact page"""
+    return render(request, 'home/contact.html')
